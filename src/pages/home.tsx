e8db84cd318943a3d6f0ce1ffa45d45fe5285f5d
이민경(Minkyung Lee) <87940552+Minlee01@users.noreply.github.com>
2025-01-21 18:37:53 +0900

    [feat]오늘의 주식 공부하기 컴포넌트 추가, 알림 창 수정

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,12 +3,13 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import '../style/home.css';
 import UserField from "../components/user_field";
+import Learn_new from "../components/learn_today";
 const Home: React.FC = () => {
     return (
         <div className="home-container">
             <Header title="홈"/>
-            <main className="home-compoent-container">
-                이지스톡
+            <main className="home-component-container">
+                <Learn_new/>
                 <UserField/>
             </main>
            <Footer/>
