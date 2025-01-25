6d5a71b1a27930450c33ce74045879a82ed27613
이민경(Minkyung Lee) <87940552+Minlee01@users.noreply.github.com>
2025-01-25 20:33:27 +0900

    [Feat]주식 정보 컴포넌트(#11)

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -4,6 +4,7 @@ import Footer from "../components/footer";
 import '../style/mypage.css';
 import UserField from "../components/user_field";
 import Open_Report from "../components/open_report";
+import StockInfo from "../components/stock_info";
 
 const MyPage: React.FC = () => {
     return (
@@ -16,6 +17,12 @@ const MyPage: React.FC = () => {
                     내가 투자한 주식 현황
                 </h3>
                 <div className="stock-componet-container">
+                    <StockInfo stockName="삼성전자" stokenValue={60300} changePercentage={10.3} />
+                </div>
+                <h3 className="title">
+                    북마크한 관심 기업
+                </h3>
+                <div className="bookmark-stock-container">
                     
                 </div>
             </main>
