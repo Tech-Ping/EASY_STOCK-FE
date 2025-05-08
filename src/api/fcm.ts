9d119bd06d17dbbf1b7926e68fedc1a182596b17
이민경(Minkyung Lee) <87940552+Minlee01@users.noreply.github.com>
2025-05-08 18:13:23 +0900

    [feat] SDK추가 (#23)

--- src/api/fcm.ts
+++ src/api/fcm.ts
@@ -1,5 +1,31 @@
 import axios from "axios";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.FCM_APIKEY,
+  authDomain: process.env.FCM_AUTHDOMAIN,
+  projectId: process.env.FCM_PROJID,
+  storageBucket: process.env.FCM_STORAGEBUCKET,
+  messagingSenderId: process.env.FCM_MSGSENDERID,
+  appId: process.env.FCM_APPID,
+  measurementId: process.env.FCM_MEAUREMENTID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+
+
 const API_BASE_URL = process.env.REACT_APP_API_URL; // .env에 설정한 백엔드 도메인
 
 // FCM 푸시 알림 테스트 API 호출 함수
